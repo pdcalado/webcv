5f2f3c4acbc64fbf1bc1311dc67973ae79e2bd4d
Pedro Calado <pdcalado@gmail.com>
2018-07-29 12:57:44 +0100

    Add pseudo personal cv data.

---
+++ .pseudo-data.js
@@ -0,0 +1,62 @@
+export default {
+    owner: {
+	name: "Lorem Ipsum",
+	title: "Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit.",
+	social: {
+	    linkedin: "https://www.linkedin.com",
+	    github: "https://github.com"
+	}
+    },
+    about: {
+	title: "Who am I?",
+	text: "Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit..<br /><br /> Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit..<br /><br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi at quam maximus condimentum. Duis sollicitudin, lorem eu ultricies tincidunt, eros purus vehicula sem, rutrum elementum nunc urna ac ligula. Morbi a posuere justo. In eleifend leo at lorem pharetra lobortis. Nulla facilisi. Nulla facilisi. Mauris lectus nulla, dapibus eu ex vitae, cursus tempus odio. Praesent efficitur, est sit amet egestas feugiat, ante neque volutpat ipsum, et suscipit odio purus vitae metus. Aliquam porttitor justo sit amet facilisis laoreet."
+    },
+    career: {
+	title: "Career",
+	stages: [
+	    {
+		entity: "Lorem ipsum",
+		dates: "dapibus eu ex vitae 2017",
+		location: "amet facilisis laoreet"
+	    },
+	    {
+		entity: "Lorem ipsum",
+		dates: "dapibus eu ex vitae 2017",
+		location: "amet facilisis laoreet"
+	    },
+	    {
+		entity: "Lorem ipsum",
+		dates: "dapibus eu ex vitae 2017",
+		location: "amet facilisis laoreet"
+	    },
+	    {
+		entity: "Lorem ipsum",
+		dates: "dapibus eu ex vitae 2017",
+		location: "amet facilisis laoreet",
+		role: "dapibus eu ex vitae 2017",
+		education: true
+	    }
+	]
+    },
+    honors: {
+	title: "Honors",
+	list: [
+	    {
+		title: "Lorem ipsum",
+		date: "2014",
+		entity: "amet facilisis."
+	    },
+	    {
+		title: "Lorem ipsum - Performance Review",
+		date: "2014",
+		entity: "amet facilisis."
+	    },
+	    {
+		title: "In eleifend leo at lorem pharetra lobortis",
+		date: "2018",
+		entity: "amet facilisis",
+		link: "https://ieeexplore.ieee.org"
+	    }
+	]
+    }
+}
