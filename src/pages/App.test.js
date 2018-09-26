3467d7e679f2232fd3e7007c7f945bd626d2f478
Pedro Calado <pdcalado@gmail.com>
2018-09-26 19:57:56 +0100

    Fix test by importing data.

--- src/pages/App.test.js
+++ src/pages/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import data from 'data.js';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    ReactDOM.render(<App data={data} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
