653fb72113a563a6dd00ce14f81c624252e4dab7
Pedro Calado <pdcalado@gmail.com>
2018-10-02 19:59:20 +0100

    Use different icon if founder.

--- src/components/intro/Timeline.js
+++ src/components/intro/Timeline.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Timeline, TimelineEvent } from 'react-event-timeline';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faBriefcase,
-    faGraduationCap
+    faBriefcase as jobIcon,
+    faGraduationCap as eduIcon,
+    faRocket as founderIcon
 } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import { colorTitle } from 'style.js';
 
 const TimelineCareer = ({ stages }) => {
     const itemList = stages.map((item, index) => {
-        const icon = item.education ? faGraduationCap : faBriefcase;
+        const icon = item.education
+            ? eduIcon
+            : item.founder
+                ? founderIcon
+                : jobIcon;
 
         const tsty = {
             fontWeight: 'bold',
