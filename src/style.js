7e9005ce5b6e013efea4f56c4ca4d7558d437957
Pedro Calado <pdcalado@gmail.com>
2018-10-10 21:36:28 +0100

    Make header padding a css variable.

--- src/style.js
+++ src/style.js
@@ -6,3 +6,6 @@ export const colorLight = style.getPropertyValue('--main-grey');
 export const colorLighter = style.getPropertyValue('--main-wgrey');
 export const colorDark = style.getPropertyValue('--main-dark');
 export const colorWhite = style.getPropertyValue('--main-white');
+
+// Paddings
+export const headerPad = style.getPropertyValue('--header-pad');
