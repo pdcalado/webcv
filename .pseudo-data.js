db0fe1b69d0d9974dafa668f8d5050579973d7c5
Pedro Calado <pedrocalado@metric4.io>
2019-12-19 10:13:22 +0000

    Indent file.

--- .pseudo-data.js
+++ .pseudo-data.js
@@ -1,145 +1,145 @@
 export default {
-    owner: {
-	name: "Lorem Ipsum",
-	title: "Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit.",
-	social: {
-	    linkedin: "https://www.linkedin.com",
-	    github: "https://github.com"
-	}
-    },
-    about: {
-	title: "Who am I?",
-	text: "Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit..<br /><br /> Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit..<br /><br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi at quam maximus condimentum. Duis sollicitudin, lorem eu ultricies tincidunt, eros purus vehicula sem, rutrum elementum nunc urna ac ligula. Morbi a posuere justo. In eleifend leo at lorem pharetra lobortis. Nulla facilisi. Nulla facilisi. Mauris lectus nulla, dapibus eu ex vitae, cursus tempus odio. Praesent efficitur, est sit amet egestas feugiat, ante neque volutpat ipsum, et suscipit odio purus vitae metus. Aliquam porttitor justo sit amet facilisis laoreet."
-    },
-    career: {
-	title: "Career",
-	stages: [
-	    {
-		entity: "Lorem ipsum",
-		dates: "dapibus eu ex vitae 2017",
-		location: "amet facilisis laoreet"
-	    },
-	    {
-		entity: "Lorem ipsum",
-		dates: "dapibus eu ex vitae 2017",
-		location: "amet facilisis laoreet"
-	    },
-	    {
-		entity: "Lorem ipsum",
-		dates: "dapibus eu ex vitae 2017",
-		location: "amet facilisis laoreet"
-	    },
-	    {
-		entity: "Lorem ipsum",
-		dates: "dapibus eu ex vitae 2017",
-		location: "amet facilisis laoreet",
-		role: "dapibus eu ex vitae 2017",
-		education: true
-	    }
-	]
-    },
-    honors: {
-	title: "Honors",
-	list: [
-	    {
-		title: "Lorem ipsum",
-		date: "2014",
-		entity: "amet facilisis."
-	    },
-	    {
-		title: "Lorem ipsum - Performance Review",
-		date: "2014",
-		entity: "amet facilisis."
-	    },
-	    {
-		title: "In eleifend leo at lorem pharetra lobortis",
-		date: "2018",
-		entity: "amet facilisis",
-		link: "https://ieeexplore.ieee.org"
-	    }
-	]
-    },
-    skills: {
-	title: "Skills",
-	programming: {
-	    title: "Programming Skills",
-	    list: [
-		{
-		    name: "Golang",
-		    value: 10,
-		    date: "2018-01"
-		},
-		{
-		    name: "Python",
-		    value: 20,
-		    date: "2018-01"
-		},
-		{
-		    name: "C",
-		    value: 30,
-		    date: "2018-01"
-		},
-		{
-		    name: "C++",
-		    value: 40,
-		    date: "2018-01"
-		}
-	    ],
-          description: "The programming languages I've learned"
-	},
-	tools: {
-	    title: "Tools Proficiency",
-	    list: [
-		{
-		    name: "Linux",
-		    value: 60,
-		    date: "2017-01"
-		},
-		{
-		    name: "Git",
-		    value: 60,
-		    date: "2017-01"
-		},
-		{
-		    name: "Docker",
-		    value: 60,
-		    date: "2017-01"
-		},
-		{
-		    name: "Ansible",
-		    value: 60,
-		    date: "2017-01"
-		},
-		{
-		    name: "ZeroMQ",
-		    value: 60,
-		    date: "2017-01"
-		}
-	    ],
-          description: "The tools I've learned"
-	},
-	languages: {
-	    title: "Languages Spoken",
-	    list: [
-		{
-		    name: "English",
-		    value: 90
-		},
-		{
-		    name: "French",
-		    value: 100
-		},
-		{
-		    name: "Japanese",
-		    value: 50
-		}
-	    ],
-            description: "The languages I've learned"
-	}
-    },
-    contact: {
-	message: "You can find the source code for this webiste at <a href=\"https://github.com/pdcalado/webcv\">webcv</a>.",
-	policy: "This website does not use cookies or gather any sort of information from it's visitors.",
-	copy: "&copy; 2018 Pedro Calado - all rights reserved."
+  owner: {
+    name: "Lorem Ipsum",
+    title: "Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit.",
+    social: {
+      linkedin: "https://www.linkedin.com",
+      github: "https://github.com"
     }
-}
+  },
+  about: {
+    title: "Who am I?",
+    text: "Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit..<br /><br /> Lorem ipsum dolor sit amet,  <strong> consectetur </strong> adipiscing elit..<br /><br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id nisi at quam maximus condimentum. Duis sollicitudin, lorem eu ultricies tincidunt, eros purus vehicula sem, rutrum elementum nunc urna ac ligula. Morbi a posuere justo. In eleifend leo at lorem pharetra lobortis. Nulla facilisi. Nulla facilisi. Mauris lectus nulla, dapibus eu ex vitae, cursus tempus odio. Praesent efficitur, est sit amet egestas feugiat, ante neque volutpat ipsum, et suscipit odio purus vitae metus. Aliquam porttitor justo sit amet facilisis laoreet."
+  },
+  career: {
+    title: "Career",
+    stages: [
+      {
+        entity: "Lorem ipsum",
+        dates: "dapibus eu ex vitae 2017",
+        location: "amet facilisis laoreet"
+      },
+      {
+        entity: "Lorem ipsum",
+        dates: "dapibus eu ex vitae 2017",
+        location: "amet facilisis laoreet"
+      },
+      {
+        entity: "Lorem ipsum",
+        dates: "dapibus eu ex vitae 2017",
+        location: "amet facilisis laoreet"
+      },
+      {
+        entity: "Lorem ipsum",
+        dates: "dapibus eu ex vitae 2017",
+        location: "amet facilisis laoreet",
+        role: "dapibus eu ex vitae 2017",
+        education: true
+      }
+    ]
+  },
+  honors: {
+    title: "Honors",
+    list: [
+      {
+        title: "Lorem ipsum",
+        date: "2014",
+        entity: "amet facilisis."
+      },
+      {
+        title: "Lorem ipsum - Performance Review",
+        date: "2014",
+        entity: "amet facilisis."
+      },
+      {
+        title: "In eleifend leo at lorem pharetra lobortis",
+        date: "2018",
+        entity: "amet facilisis",
+        link: "https://ieeexplore.ieee.org"
+      }
+    ]
+  },
+  skills: {
+    title: "Skills",
+    programming: {
+      title: "Programming Skills",
+      list: [
+        {
+          name: "Golang",
+          value: 10,
+          date: "2018-01"
+        },
+        {
+          name: "Python",
+          value: 20,
+          date: "2018-01"
+        },
+        {
+          name: "C",
+          value: 30,
+          date: "2018-01"
+        },
+        {
+          name: "C++",
+          value: 40,
+          date: "2018-01"
+        }
+      ],
+      description: "The programming languages I've learned"
+    },
+    tools: {
+      title: "Tools Proficiency",
+      list: [
+        {
+          name: "Linux",
+          value: 60,
+          date: "2017-01"
+        },
+        {
+          name: "Git",
+          value: 60,
+          date: "2017-01"
+        },
+        {
+          name: "Docker",
+          value: 60,
+          date: "2017-01"
+        },
+        {
+          name: "Ansible",
+          value: 60,
+          date: "2017-01"
+        },
+        {
+          name: "ZeroMQ",
+          value: 60,
+          date: "2017-01"
+        }
+      ],
+      description: "The tools I've learned"
+    },
+    languages: {
+      title: "Languages Spoken",
+      list: [
+        {
+          name: "English",
+          value: 90
+        },
+        {
+          name: "French",
+          value: 100
+        },
+        {
+          name: "Japanese",
+          value: 50
+        }
+      ],
+      description: "The languages I've learned"
+    }
+  },
+  contact: {
+    message: "You can find the source code for this webiste at <a href=\"https://github.com/pdcalado/webcv\">webcv</a>.",
+    policy: "This website does not use cookies or gather any sort of information from it's visitors.",
+    copy: "&copy; 2018 Pedro Calado - all rights reserved."
+  }
+};
